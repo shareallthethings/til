589181a33aef2a9741376563a46f24419594dcb1
Erich Kist <erichkist@gmail.com>
2015-12-19 04:05:31 -0200

    Copy the url to share to clipboard or redirect the user

--- _assets/javascripts/application.js
+++ _assets/javascripts/application.js
@@ -3,3 +3,24 @@
 //= require zepto/fx_methods
 //= require plugins/ohsnap.min
 //= require plugins/clipboard.min
+
+var clipboard = new Clipboard('.permalink');
+
+clipboard.on('success', function(e) {
+  //Give a feedback to the user.
+  ohSnap("Link copied to your clipboard", { duration: '2000', color: 'green' });
+  setTimeout(function(){
+    window.open(e.text, '_blank')
+  }, 2100);
+});
+
+clipboard.on('error', function(e) {
+  // Safari not support it. Open the link in a new tab.
+  ohSnap("This browser does not supported the copy. Redirecting...", { duration: '2000', color: 'red' });
+
+  setTimeout(function(){
+    var url = $(e.trigger).data('clipboard-text');
+
+    window.location.href = url;
+  }, 2000);
+});
